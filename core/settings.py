1e0a9da75a5e890baee2749efc2fc19ff9ac7f29
mufasa101 <joseph.omenda007@gmail.com>
2021-09-29 10:54:46 +0300

    allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','disraeili-google-apis.herokuapp.com']
 
 # Application definition
 
